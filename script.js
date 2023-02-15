f0d5bc70f2723c9e083267d152e9f3a01a2ab668
Gokcenomer <b.ylmz55@hotmail.com>
2023-02-15 18:37:39 +0300

    Refactored functions

--- script.js
+++ script.js
@@ -1,51 +1,40 @@
+const container = document.querySelector(".container");
+const rowsCountButton = document.querySelector("#rowCount");
 
+const defaultRowsCount = 4;
 
-const container = document.querySelector('.container');
-const rowsCount = document.querySelector('#rowCount');
+const getUserInput = () => {
+  rows = prompt("Enter a number smaller then 10");
+  while (rows >= 10) {
+    rows = prompt("Enter a number smaller then 10");
+  }
+  if (rows == null) return defaultRowsCount;
 
+  return rows;
+};
 
-let rows = 4;
+function makeRows(rows) {
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", rows);
 
-function makeRows(rows){
+  for (let i = 0; i < rows * rows; i++) {
+    let squares = document.createElement("div");
 
-container.style.setProperty('--grid-rows',rows);
-container.style.setProperty('--grid-cols',rows);
-
-for (let i = 0; i < rows * rows; i++) {
-    
-    let squares = document.createElement('div');
-    
-    container.appendChild(squares).className = 'grid-item';
-
-    
+    container.appendChild(squares).className = "grid-item";
+  }
 }
 
-
-}
-
-rowsCount.addEventListener('click' ,() => {
-
-    
-    rows = prompt('Enter a number smaller then 10');
-    
-
-    while (rows >= 100){
-        rows = prompt('Enter a number smaller then 10');
-    }
-    container.replaceChildren();
-    makeRows(rows);
-  
-
+rowsCountButton.addEventListener("click", () => {
+  getUserInput();
+  container.replaceChildren();
+  makeRows(getUserInput());
 });
 
-
-
-container.addEventListener('mouseover' ,(e) => {
-    let red = (Math.floor(Math.random() * 256));
-    let green = (Math.floor(Math.random() * 256));
-    let blue = (Math.floor(Math.random() * 256));
-    e.target.style.background = `rgb(${red}, ${green} , ${blue})`;
-    
-
+container.addEventListener("mouseover", (e) => {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  e.target.style.background = `rgb(${red}, ${green} , ${blue})`;
 });
-makeRows(rows);
+
+makeRows(getUserInput());
